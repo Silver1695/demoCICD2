7f1db1aa99dd5f3805a63be71c387d59a7871cdd
Thang <thang16092005@gmail.com>
2025-04-13 23:55:47 +0700

    test fail

--- src/test/java/TestCaculator.java
+++ src/test/java/TestCaculator.java
@@ -7,12 +7,12 @@ public class TestCaculator {
     @Test
     public void testSum() {
         Caculator caculator = new Caculator();
-        Assert.assertEquals(caculator.sum(2,3),5);
+        Assert.assertEquals(caculator.sum(5,9),14);
     }
 
     @Test
     public void testSub() {
         Caculator caculator = new Caculator();
-        Assert.assertEquals(caculator.sub(2,2),0);
+        Assert.assertEquals(caculator.sub(3,3),0);
     }
 }
