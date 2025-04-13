c26c65b76982433909c3bbf09acf54b2e9061158
Thang <thang16092005@gmail.com>
2025-04-13 23:46:04 +0700

    first commit 2

---
+++ src/main/java/org/example/Caculator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Caculator {
+    public int sum(int a, int b) {
+        return 0;
+    }
+    public int sub(int a, int b) {
+        return 0;
+    }
+}
---
+++ src/main/java/org/example/Icaculator.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public interface Icaculator {
+    Integer sum(int a, int b);
+    Integer sub(int a, int b);
+}
